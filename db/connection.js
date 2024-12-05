8fe6b333ac8f722bee9762ec49d6d54daf254817
Izekki <jaldair2011@hotmail.com>
2024-12-05 15:01:00 -0600

    last commit

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,15 @@
+// Cargar las variables de entorno desde el archivo .env
+require('dotenv').config();
+
 const mysql = require('mysql2');
 
-// Crear conexión a la base de datos
+// Crear conexión a la base de datos utilizando las variables de entorno
 const connection = mysql.createConnection({
-    host: 'localhost',      
-    user: 'root',       
-    password: 'password2024', 
-    database: 'prestamouv'
+    host: process.env.DB_HOST,      
+    port: process.env.DB_PORT,      
+    user: process.env.DB_USER,      
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_NAME  
 });
 
 // Conectar a la base de datos
